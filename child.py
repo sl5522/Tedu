a4696fcf46dcf3439a41a106259d1f6b8032bc7b
sunlei <102264591@qq.com>
2021-07-07 20:17:44 -0700

    add

---
+++ child.py
@@ -0,0 +1,27 @@
+import os
+from time import sleep
+
+
+def f1():
+    sleep(2)
+    print("fun1")
+
+
+def f2():
+    sleep(3)
+    print("fun2")
+
+
+pid = os.fork()
+
+if pid < 0:
+    print("Error")
+elif pid == 0:
+    pid_0 = os.fork()  # 二级的子进程
+    if pid_0 == 0:
+        f1()  # 二级子进程执行
+    else:
+        os._exit(0)
+else:
+    os.wait()
+    f2()
